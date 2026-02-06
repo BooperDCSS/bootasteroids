6911d7b51f8c91262e93217d67ca6edc6c787d3e
booper <otosound@protonmail.com>
2026-02-05 21:37:00 -0500

    implemented forward and backward movement of sprite

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player1.update(dt)
+
         screen.fill("black")
+        player1.update(dt)
         player1.draw(screen)
 
         pygame.display.flip()
